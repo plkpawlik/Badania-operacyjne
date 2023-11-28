eb71b1683679b405276608e4990854d34698e263
kpawlik <kamil.pawlik@capgemini.com>
2023-11-28 14:06:39 +0100

    LAB_04 Start

---
+++ lab_04/src/functions.ts
@@ -0,0 +1,32 @@
+export const BASE_ARRIVAL_TIME = 120;
+export const BASE_SERVICE_TIME = 60;
+export const BASE_STD_DEV = 20;
+
+// exponential distribution: E(120)
+export const nextArrivalTime = (): number => {
+	return -BASE_ARRIVAL_TIME * Math.log(1 - Math.random());
+};
+
+// constant distribution: C(60)
+export const nextServiceTimeA = (): number => {
+	return BASE_SERVICE_TIME;
+};
+
+// uniform distribution: U(0, 120)
+export const nextServiceTimeB = (): number => {
+	return Math.round(BASE_SERVICE_TIME * Math.random());
+};
+
+// exponential distribution: E(60)
+export const nextServiceTimeC = (): number => {
+	return -BASE_SERVICE_TIME * Math.log(1 - Math.random());
+};
+
+// normal distribution: N(60, 20)
+export const nextServiceTimeD = (): number => {
+	const u = 1 - Math.random();
+	const v = Math.random();
+	const z = Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+
+	return Math.max(0, z * BASE_STD_DEV + BASE_SERVICE_TIME);
+};
