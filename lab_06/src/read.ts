5bb33e1dae961fa902df2454ff15f48e657b2e02
kawlik <kpawlik1103@gmail.com>
2024-01-16 19:45:49 +0100

    LAB_06 Działa i bajlando (xD)

--- lab_06/src/read.ts
+++ lab_06/src/read.ts
@@ -1,14 +1,20 @@
 import { EOL } from "https://deno.land/std@0.212.0/fs/eol.ts";
 
-export function read(path: string): number[][] {
-	const lines = Deno.readTextFileSync(path).split(EOL);
+export function read(path: string): { cost: number; gain: number }[][] {
+	const data = Deno.readTextFileSync(path).split("data:");
+	const buff = new Array<{ cost: number; gain: number }[]>(data.length - 1);
 
-	const size = Number.parseInt(lines[0].split("data:")[1]);
-	const data = lines.slice(1, size + 1).map((line) => {
-		return line.split(" ").filter((char) => char.length).map((char) => {
-			return Number.parseInt(char);
+	for (let i = 1; i < data.length; i++) {
+		const lines = data[i].split(EOL);
+		const size = lines[0].split(" ");
+
+		buff[i] = lines.slice(1, 1 + Number.parseInt(size[1])).map((line) => {
+			const cost = Number.parseInt(line.substring(0, 2));
+			const gain = Number.parseInt(line.substring(3, 5));
+
+			return ({ cost, gain });
 		});
-	});
+	}
 
-	return data;
+	return buff.slice(1);
 }
